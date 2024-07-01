8f99692cbe9b602d059c2fe4713488832a69fa1e
Archie Butler <archie_butler@hotmail.co.uk>
2024-07-01 09:35:41 +0100

    Test commit

--- Stocklite1/Form1.Designer.cs
+++ Stocklite1/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(183)))), ((int)(((byte)(195)))));
+            this.button1.CausesValidation = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(954, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(705, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseMnemonic = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(183)))), ((int)(((byte)(195)))));
+            this.button2.CausesValidation = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Consolas", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(954, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(705, 92);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Help";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(25)))), ((int)(((byte)(82)))));
+            this.ClientSize = new System.Drawing.Size(2493, 1658);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "StockLite";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Stocklite1/Form1.cs
+++ Stocklite1/Form1.cs
@@ -15,6 +15,34 @@ namespace Stocklite1
         public Form1()
         {
             InitializeComponent();
+            
+            this.Resize += new EventHandler(Form1_Resize);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            CenterButton();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            
+            CenterButton();
+        }
+
+        private void CenterButton()
+        {
+            
+            button1.Left = (this.ClientSize.Width - button1.Width) / 2;
+            button1.Top = (this.ClientSize.Height - button1.Height) / 3;
+            button2.Left = (this.ClientSize.Width - button1.Width) / 2;
+            button2.Top = (this.ClientSize.Height - button1.Height) / 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
