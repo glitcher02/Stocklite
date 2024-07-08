4c78c7fb8167e55e3c36f22b3f437038bb7060eb
Archie Butler <archie_butler@hotmail.co.uk>
2024-07-08 09:19:14 +0100

    Added a API grab from polygon.io that saves to file appledata.json
    it does this in the loading page.
    
    more apis to be added

--- Stocklite1/Form3.Designer.cs
+++ Stocklite1/Form3.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,6 +57,10 @@
             this.label1.Text = "Loading...";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
             // LoadingPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
@@ -75,5 +81,6 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Stocklite1/Program.cs
+++ Stocklite1/Program.cs
@@ -3,31 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+using System.Security.Policy;
 
 namespace Stocklite1
 {
     static class Program
     {
+        private static readonly HttpClient client = new HttpClient();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static async Task Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            
+            string appleapiUrl = "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-02-10?adjusted=true&sort=asc&apiKey=IuP6zivLRuSGEb2N_nPt2XRde0pOH3_m";
+
             Stocklite1.LoadingPage loadingForm = new Stocklite1.LoadingPage();
+
             loadingForm.Show();
+            Console.WriteLine("hello");
+            WebClient wc = new WebClient();
+            string content = wc.DownloadString(appleapiUrl);
 
-            
-            System.Threading.Tasks.Task.Delay(3000).Wait(); // 3 seconds
+            Console.WriteLine(content);
+                string filePath = "C:\\Users\\archie\\source\\repos\\Stocklite1\\Stocklite1\\Resources\\appledata.json";
 
-            
+                
+            File.WriteAllText(filePath, content);
+
+            Console.WriteLine("Data saved to " + filePath);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Failed to fetch data. Status code: " + response.StatusCode);
+            //}
+
+            //System.Threading.Tasks.Task.Delay(3000).Wait(); // 3 seconds
+
             loadingForm.Close();
 
-            
             Application.Run(new Form1());
         }
     }
